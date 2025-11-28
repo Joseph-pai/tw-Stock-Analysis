389b43a22e239b7bd703549351332733f61ca380
Joseph-pai <a18911289122@gmail.com>
2025-11-28 23:06:39 +0800

    Add files via upload

--- netlify/functions/structured-analyze.js
+++ netlify/functions/structured-analyze.js
@@ -3,18 +3,6 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context) {
   console.log('=== 結構化分析開始 ===');
   
-  if (event.httpMethod === 'OPTIONS') {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-      },
-      body: ''
-    };
-  }
-
   try {
     const { stockId, stockName, apiKey, analysisType, platform = 'deepseek' } = JSON.parse(event.body || '{}');
     
